99e479a76db76af32a9aca2b3888767ad55dac84
rogera <wolfgang.schuessel@gmail.com>
2017-01-05 22:52:03 +0100

    initial for this branch

---
+++ app/src/test/java/com/example/rogera/myapplication/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package com.example.rogera.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        System.out.println("it seems to be true, 2+2 is 4.");
+        assertEquals(4, 2 + 2);
+    }
+}
